9fe247f70d729a7603e1e84dbdadca036e798362
Mgamalelsayed <mgamalelsayed.1@gmail.com>
2025-10-15 20:56:19 +0300

    testing change

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from a more advanced Docker container!'
+    return 'Hello from a more advanced Docker container! CHANGED!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
