aea49c4a03c11b0b300b064bbb653ac8cf412c03
Mgamalelsayed <mgamalelsayed.1@gmail.com>
2025-10-15 21:00:33 +0300

    testing adding new branch

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from a more advanced Docker container! CHANGED!'
+    return 'Hello from a more advanced Docker container! CHANGED! TWICE!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
